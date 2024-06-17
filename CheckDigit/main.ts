17de488286f28d26059bfd8b18f017ae948ee692
Genilson do Carmo <genilsondocarmosilva@gmail.com>
2024-06-17 00:57:38 -0300

    🏧 Operações Bancarias💰
    Aplicação feita em JAVA e refatorada para TypeScript

---
+++ CheckDigit/main.ts
@@ -0,0 +1,14 @@
+function createCheckDigit(membershipId: string): number {
+    while (membershipId.length > 1) {
+        membershipId = membershipId
+            .split('')
+            .reduce((acc: number, valor: string) => acc + parseInt(valor, 10), 0)
+            .toString();
+    }
+
+    return parseInt(membershipId, 10);
+}
+
+// console.log(createCheckDigit("55555"));
+// console.log(createCheckDigit("122"));
+console.log(createCheckDigit("9998888745477878"));
