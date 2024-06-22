843e58bf22a244a14f260ea06d400b5ced9e844c
Genilson do Carmo <genilsondocarmosilva@gmail.com>
2024-06-22 01:34:46 -0300

    Test for FormatDate

---
+++ FormatDate/src/main.ts
@@ -0,0 +1,13 @@
+export function formatDate(userDate: string): string {
+  const [month, day, year] = userDate.split('/');
+  if (month === undefined || day === undefined || year === undefined) {
+    throw new Error('invalid date format! \n Please use MM/DD/YYYY');
+  }
+  const formattedDate = `${year}${month.padStart(2, '0')}${day.padStart(
+    2,
+    '0'
+  )}`;
+  return formattedDate;
+}
+
+console.log(formatDate('12/31/2014'));
